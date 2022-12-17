3ec600dec041661587b9f9e53be6788a064f33f0
methokb <abdurasulovrahmatso@gmail.com>
2022-12-17 17:03:56 +0300

    dop4

---
+++ main.js
@@ -0,0 +1,20 @@
+// let task1 = () => {
+//     return document.querySelector("li"). textContent
+// };
+// console.log(task1);
+
+// let task2 = () => {
+//     let list = Array.from(document.querySelectorAll("li"));
+//     return list.map(li => li.textContent);
+// }
+
+// let task3 = () => {
+//     let listItem = Array.from(document.querySelectorAll("li.list-item"))
+    // return listItem.map(li => li.textContent);
+// }
+
+
+// let task4 = () => {
+//     let listItem = elem.getAttribute(document.querySelectorAll("li.list-item"))
+//     return listItem.map(li => li.textContent);
+// }
